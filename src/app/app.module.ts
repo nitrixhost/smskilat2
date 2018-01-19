febb6e5b2dd5aa4075d84ff8470be050b033e158
Alfin Syamsuddin <nitrixhost@gmail.com>
2018-01-19 08:22:44 +0700

    perubahan pertama

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router'; 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SmsComponent } from './sms/sms.component';
+import { KontakComponent } from './kontak/kontak.component';
 
+
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'sms', component: SmsComponent},
+  {path: 'kontak', component: KontakComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SmsComponent,
+    KontakComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sms/sms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sms',
+  templateUrl: './sms.component.html',
+  styleUrls: ['./sms.component.less']
+})
+export class SmsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
